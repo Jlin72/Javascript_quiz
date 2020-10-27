407294a225974a3d2dc14d512b6952b4f491ae90
Jlin72 <67341179+Jlin72@users.noreply.github.com>
2020-10-27 19:02:42 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,54 +3,55 @@ var startEl = document.querySelector("#start");
 var quizEl = document.querySelector("#quiz");
 var quizQuestionsEl = document.querySelector("#quizQuestions");
 var choices = document.querySelector ("#choices");
-var liEl = document.querySelector ("#li");
 var answerResponseEl = document.querySelector ("#answerResponse");
 var h1 = document.getElementById ("h1");
 var scoreH1 = document.querySelector ("#scoreH1");
 var quizIntroEl = document.querySelector ("#quizIntro");
 var scoreInfo = document.querySelector ("#scoreInfo");
 var answerValue = document.querySelector("#answer");
-var score = 0;
+var optionA = document.querySelector("#choiceA");
+var optionB = document.querySelector("#choiceB");
+var optionC = document.querySelector("#choiceC");
+var optionD = document.querySelector("#choiceD");
 var timeLeft = 80;
-// function for the timer
-// var timeInterval = setInterval(function () {
-//     timeLeft --;
-//     document.getElementById("countdown").innerHTML = "Time left: "+ timeLeft;
-//     if (timeLeft <=0) {
-//       clearInterval(timeinterval);
-//       document.getElementById("countdown").innerHTML = "Time out!";
-//     }
-//   }, 1000);
-
-// start.addEventListener("click", timeInterval);
 
 // This is the var we will be using to contain the questions for the quiz
 let quizQuestions = [{
-  "question":"Who first designed javaScript?",
-  "choices":["Albert Einstein", "Bill Gates", "Steve Jobs", "Brendan Eich"],
-  "correctAnswer":"Brendan Eich",
+  question:"Who first designed javaScript?",
+  choiceA:"Albert Einstein",
+  choiceB:"Bill Gates",
+  choiceC:"Steve Jobs",
+  choiceD:"Brendan Eich",
+  correctAnswer:"D"
 }, {
-  "question":"What is the main purpose of javaScript?",
-  "choices":["javaScript helps with creating the backbone of the website", "javaScript is used to make create a GPS for a car", "javaScript is usually used to make websites more interactive", "javaScript is usually used to make websites look more colorful and interesting"],
-  "correctAnswer":"javaScript is usually used to make websites more interactive",
+  question:"What is the main purpose of javaScript?",
+  choiceA:"javaScript helps with creating the backbone of the website",
+  choiceB:"javaScript is used to make create a GPS for a car",
+  choiceC:"javaScript is usually used to make websites more interactive",
+  choiceD:"javaScript is usually used to make websites look more colorful and interesting",
+  correctAnswer:"C",
 }, {
-  "question":"What would be the correct HTML tag for javascript?",
-  "choices":["<script>", "<nav>", "<body>", "<link>"],
-  "correctAnswer":"<script>",
-}];
-
-
-// the next function will help with the display of the question aspect of quizQuestions
+  question:"What would be the correct HTML tag for javascript?",
+  choiceA:"script",
+  choiceB:"nav",
+  choiceC:"body",
+  choiceD:"link",
+  correctAnswer:"A"
+},];
+// this function will be used later
+var  questionIndex=0;
+// the next function will help with the display of the questions
 function displayQuestion () {
-  for (i=0; i<quizQuestions.length; i++){
-  var q = quizQuestions[i];
-  quizQuestionsEl.innerHTML = q.question;
-  var button = document.createElement("button");
-  button.textContent= q.choices;
-  liEl.appendChild(button);
-  choices.setAttribute("style", "block;");
+  var q = quizQuestions[questionIndex];
+    quizQuestionsEl.innerHTML = q.question;
+    optionA.innerHTML = q.choiceA;
+    optionB.innerHTML = q.choiceB;
+    optionC.innerHTML = q.choiceC;
+    optionD.innerHTML = q.choiceD;
+    quizEl.style.display = "block";
   return;
-}}
+}
+
 
 // This next function will be used to start the quiz
 function quizStart () {
@@ -59,17 +60,37 @@ function quizStart () {
   quizEl.style.display = "block";
 }
 
-// This next function will be used to check wether the answer selected by the user is correct or wrong
-function answerCheck (answer) {
-  if (answer=quizQuestions.answer) {
-    answerValue.textContent = "Correct"
-    answerValue.setAttribute ("class", "answerbox")
-
-  }
-  else if (answer != quizQuestions.answer) {
-    var display
-  }
+// this next function will be used to check if the user clicked on the correct answer
+function answerCheck(answer) {
+  if (questionIndex < quizQuestions.length - 1) {
+    if (answer == quizQuestions[questionIndex].correctAnswer) {
+        questionIndex++;
+        alert("Correct!");
+        answerValue.style.display = "block";
+        setTimeout(displayQuestion,1000);
+    }
+    else {
+        timeLeft -5;
+        questionIndex++;
+        alert ('Wrong!');
+        answerValue.style.display = "block";
+        setTimeout(displayQuestion,1000);
+    }
 }
+else {
+    if (answer == quizQuestions[questionIndex].correctAnswer) {
+        answerValue.innerHTML= "Correct!"
+        answerValue.style.display = "block";
+        setTimeout(displayQuestion,1000);
+    }
+    else {
+      answerValue.innerHTML= "Wrong!"
+      answerValue.style.display = "block";
+      setTimeout(displayQuestion,1000);
+    }
+}
+}
+
 
 // this next function will help with the start of the quiz when the start button has been pressed
 startEl.addEventListener ("click", function () { var timeInterval = setInterval (function(){timeLeft --;
@@ -82,5 +103,6 @@ startEl.addEventListener ("click", function () { var timeInterval = setInterval 
 
   },1000);
   quizStart();
+  // answerCheck();
 });
 
